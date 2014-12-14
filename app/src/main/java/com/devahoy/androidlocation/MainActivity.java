b30497e069b0d96fa8a59173fe0491e33d6c2d8b
Chai Phonbopit <phonbopit@gmail.com>
2014-12-14 14:07:42 +0700

    receive location update

--- app/src/main/java/com/devahoy/androidlocation/MainActivity.java
+++ app/src/main/java/com/devahoy/androidlocation/MainActivity.java
@@ -1,23 +1,32 @@
 package com.devahoy.androidlocation;
 
+import android.content.Context;
+import android.content.Intent;
+import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.GooglePlayServicesUtil;
 import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationServices;
 
 
 public class MainActivity extends ActionBarActivity implements
         GoogleApiClient.ConnectionCallbacks,
-        GoogleApiClient.OnConnectionFailedListener {
+        GoogleApiClient.OnConnectionFailedListener,
+        LocationListener {
 
     private final String TAG = MainActivity.class.getSimpleName();
     private GoogleApiClient mClient;
+    private LocationRequest mLocationRequest;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,12 +38,30 @@ public class MainActivity extends ActionBarActivity implements
                 .addConnectionCallbacks(this)
                 .addOnConnectionFailedListener(this)
                 .build();
+
+        LocationManager locationManager = (LocationManager) getSystemService(
+                Context.LOCATION_SERVICE);
+        // Enable Location.
+        if (!locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+            Intent intent = new Intent(android.provider.Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+            startActivity(intent);
+        }
+
     }
 
     @Override
     protected void onStart() {
         super.onStart();
-        mClient.connect();
+        if (GooglePlayServicesUtil.isGooglePlayServicesAvailable(this) ==
+                ConnectionResult.SUCCESS) {
+            mClient.connect();
+        }
+    }
+
+    @Override
+    protected void onStop() {
+        mClient.disconnect();
+        super.onStop();
     }
 
     @Override
@@ -69,6 +96,15 @@ public class MainActivity extends ActionBarActivity implements
     @Override
     public void onConnected(Bundle bundle) {
         Log.d(TAG, "onConnected");
+        // Get Location without update.
+//        LocationServices.FusedLocationApi.getLastLocation(mClient);
+
+        mLocationRequest = LocationRequest.create();
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_BALANCED_POWER_ACCURACY);
+        mLocationRequest.setInterval(5000);
+
+        LocationServices.FusedLocationApi.requestLocationUpdates(
+                mClient, mLocationRequest, this);
     }
 
     @Override
@@ -79,5 +115,10 @@ public class MainActivity extends ActionBarActivity implements
     @Override
     public void onConnectionFailed(ConnectionResult connectionResult) {
         Log.d(TAG, "onConnectionFailed");
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        Toast.makeText(this, "Location : " + location.toString(), Toast.LENGTH_SHORT).show();
     }
 }
